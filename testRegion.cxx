c8d355bb9bd53a7105b349eeb15f063b1c5b5639
loopset <miguellozanogonzalez119@gmail.com>
2024-03-04 17:10:28 +0100

    advances on MultiRegion...
    but now we are testing with preliminar data given by Thomas in E750 folder

--- testRegion.cxx
+++ testRegion.cxx
@@ -1,12 +1,38 @@
+#include "ActClIMB.h"
+#include "ActDataManager.h"
+#include "ActInputIterator.h"
 #include "ActMultiRegion.h"
 #include "ActOptions.h"
+#include "ActTypes.h"
+
+#include <memory>
 
 void testRegion()
 {
     ActRoot::Options::GetInstance()->Print();
     std::cout << "Config : " << ActRoot::Options::GetInstance()->GetConfigDir() << '\n';
 
+    // Read data
+    ActRoot::DataManager datman {ActRoot::ModeType::EFilter};
+    datman.ReadDataFile(ActRoot::Options::GetInstance()->GetConfigDir() + "data.conf");
+    auto input {datman.GetInput()};
+    // wrapper
+    ActRoot::InputWrapper wrapper {&input};
+
+    auto cl {std::make_shared<ActAlgorithm::ClIMB>()};
+    cl->SetMinPoint(15);
+
     ActAlgorithm::MultiRegion mr;
     mr.ReadConfiguration();
+    mr.SetClusterPtr(cl);
     mr.Print();
+
+    while(wrapper.GoNext())
+    {
+        // wrapper.GetIt().Print();
+        // Set data
+        mr.SetTPCData(wrapper.GetTPCData());
+        // Run!
+        mr.Run();
+    }
 }
