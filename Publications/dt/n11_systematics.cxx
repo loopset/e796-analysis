e9b90c3214ed88b5e60f7895398e81e3f3dff32b
loopset <miguellozanogonzalez119@gmail.com>
2025-02-25 14:47:42 +0100

    added n = 11 and z = 8 systematics

--- Publications/dt/n11_systematics.cxx
+++ Publications/dt/n11_systematics.cxx
@@ -1,47 +1,40 @@
 #include "ActParticle.h"
 
+#include "TLine.h"
 #include "TStyle.h"
+#include "TVirtualPad.h"
 
 #include "ModelPlotter.h"
 
 #include <string>
 #include <vector>
-void z8_systematics()
+void n11_systematics()
 {
     gStyle->SetTextFont(132);
-    gStyle->SetTitleX(0.55);
     gStyle->SetTextSize(0.035);
+    gStyle->SetTitleX(0.55);
     std::vector<std::string> jpis {"5/2+", "1/2-"};
     std::vector<int> colors {8, 9};
-    // 17O(p,d)
-    PlotUtils::ModelToPlot o17pd {"^{18}O(p,d)"};
-    o17pd.SetEx({0, 3.050});
-    o17pd.SetSF({"1.31", "0.88"});
-    // 17O(p,d)
-    PlotUtils::ModelToPlot o17dt {"^{18}O(d,t)"};
-    o17dt.SetEx({0, 3.055});
-    o17dt.SetSF({"1.48(27)", "1.08"});
-    // // 16O(d,p)
-    // PlotUtils::ModelToPlot o16dp {"^{16}O(d,p)"};
-    // o16dp.SetEx({0, 3.055});
-    // o16dp.SetSF({"0.84(04)", "0.032"});
+    // 22Ne(d,t)
+    PlotUtils::ModelToPlot ne22dt {"^{22}Ne(d,t)"};
+    ne22dt.SetEx({0.351, 2.789});
+    ne22dt.SetSF({"2.16", "0.78"});
+    // 22Ne(p,d)
+    PlotUtils::ModelToPlot ne22pd {"^{22}Ne(p,d)"};
+    ne22pd.SetEx({0.350, 2.790});
+    ne22pd.SetSF({"2.5", "0.7"});
 
     // 20O(d,t)
     PlotUtils::ModelToPlot o20dt {"^{20}O(d,t)"};
     o20dt.SetEx({0, 3.141});
     o20dt.SetSF({"2.820(62)", "0.777(36)"});
 
-    // // 20O(d,p)
-    // PlotUtils::ModelToPlot o20dp {"^{20}O(d,p)"};
-    // o20dp.SetEx({0});
-    // o20dp.SetSF({"0.34(08)"});
-
     // Attach common info
     // Relative to Sn
-    ActPhysics::Particle o17 {"17O"};
+    ActPhysics::Particle ne21 {"21Ne"};
     ActPhysics::Particle o19 {"19O"};
-    std::vector<ActPhysics::Particle*> ps {&o17, &o17, &o19};
-    std::vector<PlotUtils::ModelToPlot*> models {&o17pd, &o17dt, &o20dt};
+    std::vector<ActPhysics::Particle*> ps {&o19, &ne21, &ne21};
+    std::vector<PlotUtils::ModelToPlot*> models {&o20dt, &ne22pd, &ne22dt};
     int i {};
     for(auto m : models)
     {
@@ -53,7 +46,7 @@ void z8_systematics()
     }
 
     // Init plotter
-    PlotUtils::ModelPlotter plotter {-8, 2, static_cast<int>(models.size()), "Z = 8 systematics"};
+    PlotUtils::ModelPlotter plotter {-8, 2, static_cast<int>(models.size()), "N = 11 systematics"};
     plotter.SetYaxisLabel("S_{n}^{eff} [MeV]");
     for(const auto& m : models)
         plotter.AddModel(*m);
