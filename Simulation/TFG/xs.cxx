68fb36cb155c67211592b075a8eda490fed835f1
loopset <miguellozanogonzalez119@gmail.com>
2025-06-05 10:20:02 +0200

    added xs sampling for Daniel

--- Simulation/TFG/xs.cxx
+++ Simulation/TFG/xs.cxx
@@ -1,6 +1,8 @@
 #include "TCanvas.h"
 #include "TGraphErrors.h"
 #include "TMultiGraph.h"
+#include "ActCrossSection.h"
+#include "TH1.h"
 void xs()
 {
     auto* gs {new TGraphErrors {"./Inputs/xs/s12_p1i.dat", "%lg %lg"}};
@@ -16,6 +18,22 @@ void xs()
         mg->Add(g);
     }
 
+    // Sampling
+    ActSim::CrossSection xs {};
+    xs.ReadFile("./Inputs/xs/p12_p1i.dat");
+    xs.Draw();
+
+    auto* hThetaCM {new TH1D {"hThetaCM", "CM;#theta_{CM};Counts", 300, 0, 180}};
+    for(int i = 0; i < 1000000; i++)
+    {
+        hThetaCM->Fill(xs.SampleHist());
+    }
+
+
     auto* c0 {new TCanvas {"c0", "xs canvas"}};
+    c0->DivideSquare(4);
+    c0->cd(1);
     mg->Draw("apl plc pmc");
+    c0->cd(2);
+    hThetaCM->Draw();
 }
