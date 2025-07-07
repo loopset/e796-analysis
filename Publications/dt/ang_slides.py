7bcfc6ed81118f97f845a45e33a337b727f7ce8e
loopset <miguellozanogonzalez119@gmail.com>
2025-07-07 17:53:05 +0200

    updated slides for eunpc

---
+++ Publications/dt/ang_slides.py
@@ -0,0 +1,117 @@
+from shutil import which
+import pyphysics as phys
+import matplotlib.axes as mplaxes
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
+import sys
+
+sys.path.append("../")
+sys.path.append("./")
+import styling as sty
+import dt
+
+fig = plt.figure(figsize=(4.5, 8))
+gs = gridspec.GridSpec(2, 1)
+ax_l2 = fig.add_subplot(gs[0, 0])
+ax_l0 = fig.add_subplot(gs[1, 0])
+
+# l = 2
+# ax_l2.tick_params(axis="y", which="both", right=False)
+dt.unrebin.plot_exp("g0", ax=ax_l2)
+ax_l2.set_prop_cycle(sty.cyclers["l012"])
+dt.unrebin.remove_model("g0", "l = 2 ZR 2FNR")
+dt.unrebin.plot_models("g0", ax=ax_l2)
+ax_l2.legend(loc="center right")
+
+# l = 0
+# ax_l0.yaxis.tick_right()
+dt.unrebin.plot_exp("g1", ax=ax_l0)
+ax_l0.set_prop_cycle(sty.cyclers["l012"])
+dt.unrebin.plot_models("g1", ax=ax_l0)
+
+## Annotations
+anns = [
+    ("g.s", dt.unrebin.get_model("g0", "l = 2")),
+    (dt.fit.get("g1")[0], dt.unrebin.get_model("g1", "l = 0")),
+]
+
+for i, ax in enumerate([ax_l2, ax_l0]):
+    ex, sf = anns[i]
+    ax.annotate(
+        f"{ex}" if isinstance(ex, str) else f"{ex.n:.2f} MeV",
+        xy=(0.5, 0.9),
+        xycoords="axes fraction",
+        ha="center",
+        va="center",
+        fontsize=14,
+    )
+    ax.annotate(
+        rf"C$^2$S = {sf.fSF:.2uS}",
+        xy=(0.5, 0.825),
+        xycoords="axes fraction",
+        ha="center",
+        va="center",
+        fontsize=14,
+    )
+
+# Common axis settings
+for ax in [ax_l2, ax_l0]:
+    # ax.set_yscale("log")
+    ax.set_xlim(4, 17)
+
+# Figure settings
+fig.supxlabel(r"$\theta_{\text{CM}}$ [$\circ$]", fontsize=18)
+fig.supylabel(r"d$\sigma$/d$\Omega$ [mb/sr]", fontsize=18)
+fig.tight_layout()
+fig.subplots_adjust(left=0.175, bottom=0.075)
+fig.savefig("./Outputs/ang_slide0.pdf")
+
+plt.close("all")
+
+fig, axs = plt.subplots(3, 2, sharex=True, figsize=(5, 8))
+states = ["g2", "v0", "v1", "v2", "v3"]
+handles = []
+for i, state in enumerate(states):
+    ax: mplaxes.Axes = axs.flat[i]
+    models, q = dt.assignments[state]
+    models.plot_exp(state, ax=ax)
+    ax.set_prop_cycle(sty.cyclers["l012"])
+    hand = models.plot_models(state, ax=ax)
+    if i == 0:
+        handles = hand
+    # Axis settings
+    ax.set_xlim(5, 16)
+    if i % 2 != 0:
+        ax.yaxis.tick_right()
+    if i % 2 == 0:
+        ax.tick_params(axis="y", which="both", right=False)
+    ## Annotate
+    ex, _ = dt.fit.get(state)
+    ax.annotate(
+        f"{ex.n:.2f} MeV",
+        xy=(0.5, 0.9),
+        xycoords="axes fraction",
+        ha="center",
+        va="center",
+        fontsize=14,
+    )
+    sf = models.get_model(state, "l = 1")
+    if sf is not None:
+        sf = sf.fSF
+        ax.annotate(
+            rf"C$^2$S = {sf:.2uS}",
+            xy=(0.5, 0.8),
+            xycoords="axes fraction",
+            ha="center",
+            va="center",
+            fontsize=14,
+        )
+axs.flat[-1].set_axis_off()
+axs.flat[-1].legend(handles, [h.get_label() for h in handles], loc="center")
+
+fig.tight_layout()
+fig.supxlabel(r"$\theta_{\text{CM}}$ [$\circ$]", fontsize=18)
+fig.supylabel(r"d$\sigma$/d$\Omega$ [mb/sr]", fontsize=18)
+fig.subplots_adjust(left=0.12, bottom=0.075, wspace=0.0, hspace=0.0)
+fig.savefig("./Outputs/ang_slide1.pdf")
+plt.show()
--- Publications/theory/nucleons_sm.py
+++ Publications/theory/nucleons_sm.py
@@ -1,3 +1,6 @@
+from typing import Dict
+
+from sklearn.model_selection import ValidationCurveDisplay
 import pyphysics as phys
 import matplotlib.pyplot as plt
 import matplotlib.axes as mplaxes
@@ -14,6 +17,10 @@ orbs = [
     phys.QuantumNumbers.from_str("1s1/2"),
     # phys.QuantumNumbers.from_str("0d3/2"),
 ]
+
+shift_low = +0.2
+shift_up = -0.1
+shifts = [0, shift_low, shift_up, shift_low, shift_up]
 
 # Magic numbers
 magic = [2, 8, 20]
@@ -39,7 +46,7 @@ for i, nucleons in enumerate([part.Z, part.N]):
     last_n = 0
     # aux_magic = 0
     for j, orb in enumerate(orbs):
-        y = j
+        y = j + shifts[j]
         ax.plot([xlow, xup], [y] * 2, lw=lw, color=ocolor)
         for count, nucleon in enumerate(range(last_n, nucleons)):
             if count < orb.degeneracy():
@@ -63,7 +70,8 @@ for i, nucleons in enumerate([part.Z, part.N]):
 # Orbital name and magic numbers
 cmagic = 0
 for i, orb in enumerate(orbs):
-    ax.annotate(orb.format(), xy=(1.5, i), ha="center", va="center", fontsize=14)
+    y = i + shifts[i]
+    ax.annotate(orb.format(), xy=(1.5, y), ha="center", va="center", fontsize=14)
     cmagic += orb.degeneracy()
     if cmagic in magic:
         ax.annotate(
@@ -86,4 +94,61 @@ ax.set_axis_off()
 
 fig.tight_layout()
 fig.savefig(f"./Outputs/sm_{part.symbol}.pdf")
+
+# Tiny image for xs explanation
+plt.close("all")
+fig, ax = plt.subplots(figsize=(1.5, 2.5))
+ax: mplaxes.Axes
+
+which = "n"
+ncolor = "dodgerblue" if which == "n" else "crimson"
+occupancies: Dict[phys.QuantumNumbers, int] = {
+    orbs[0]: 2,
+    orbs[1]: 4,
+    orbs[2]: 2,
+    orbs[3]: 2,
+    orbs[4]: 1,
+}
+vacancies: Dict[phys.QuantumNumbers, int] = {
+    # phys.QuantumNumbers.from_str("0d5/2"): 1,
+    phys.QuantumNumbers.from_str("1s1/2"): 1,
+}
+filename = "19O_1s12.pdf"
+print(f"Filename : {filename}")
+# X axis settings
+width = 1
+xlow = 1 - width / 2
+xup = 1 + width / 2
+for j, orb in enumerate(orbs):
+    y = j + shifts[j]
+    ax.plot([xlow, xup], [y] * 2, lw=lw, color=ocolor)
+    if orb in occupancies:
+        bw = width / orb.degeneracy()
+        count = 0
+        # Occupied
+        for n in range(occupancies[orb]):
+            xn = xlow + bw / 2 + count * bw
+            ax.plot(xn, y, color=ncolor, mfc=ncolor, marker="o", ms=8)
+            count += 1
+        # Vacancies
+        vac = 0
+        if orb.l == 1:
+            vac = orb.degeneracy() - occupancies[orb]
+        if orb in vacancies:
+            vac = vacancies[orb]
+        for h in range(vac):
+            xn = xlow + bw / 2 + count * bw
+            ax.plot(xn, y, color=ncolor, marker="o", ms=8)
+            count += 1
+
+# Orbital name and magic numbers
+for i, orb in enumerate(orbs):
+    y = i + shifts[i]
+    ax.annotate(
+        orb.format(), xy=(1 + width / 2 + 0.3, y), ha="center", va="center", fontsize=14
+    )
+ax.set_xlim(0.5, 1.8)
+ax.set_axis_off()
+fig.savefig(f"./Outputs/{filename}")
+
 plt.show()
